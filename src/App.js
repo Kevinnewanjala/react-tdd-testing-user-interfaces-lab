aac6f1ba1a320ee0462782be6fe01e2d86afcf16
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-05-03 16:39:33 +0300

    Initial comit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <main>
+      <h1>Hi, I'm Kevin</h1>
+      <img alt="This is my profile picture" src="https://scontent.fnbo1-1.fna.fbcdn.net/v/t39.30808-6/429814483_2185608555127354_259563252778425646_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeH86_2iewQtx1EdU1mh_-22gcXNK0nMVm6Bxc0rScxWbgU8VLfKadyqtMos-88-gCjB5FEEMcwt7RNYrBOis374&_nc_ohc=bdCC94vKCd8Q7kNvgHz_FNx&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfCEfjD0P44asrJiK9cwoaN6EOCY_w8i8oR3YA3H6yaTmw&oe=663AAD36" />
+      <h2>About Me</h2>
+      <p>
+        I am a student at Moringa School, determined to become a fullstack web developer by the end of this
+      </p>
+      <div>
+        <a href="https://github.com/">GitHub</a>
+        <a href="https://www.linkedin.com/feed/">LinkedIn</a>
+      </div>
+    </main>
+  );
 }
 
 export default App;
